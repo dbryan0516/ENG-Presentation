e81d09344d48acd31410a54421a6d92c7c03a322
hfernan <hfernan@ncsu.edu>
2017-11-10 18:02:06 -0500

    Improved grammar.

--- ENG331/Eng331.java
+++ ENG331/Eng331.java
@@ -1,9 +1,11 @@
 public class Eng331 {
     /**
+     * A project
      * @author Hanna Fernandez
      *
      */
+    
     public static void main(String[] args) {
-        System.out.println("I love pie");
+        System.out.println("I love pie.");
     }
 }
